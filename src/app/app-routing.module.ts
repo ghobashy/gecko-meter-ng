46bd184a3bf134a09bcca0c85ecafd819c02225c
Mohamed Ghobashy <mohamed.mostafa-ghobashy@vodafone.com>
2018-12-03 21:37:22 +0200

    [DEVELOP] Add app routing with dashboard module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    loadChildren: 'app/dashboard/dashboard.module#DashboardModule'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
