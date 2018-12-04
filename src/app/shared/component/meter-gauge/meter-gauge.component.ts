17a91e8be46a0d90c210f034f941f041b3f4485a
Mohamed Ghobashy <mohamed.mostafa-ghobashy@vodafone.com>
2018-12-04 23:19:11 +0200

    [DEVELOP] Add Meter gauge component

--- src/app/shared/component/meter-gauge/meter-gauge.component.ts
+++ src/app/shared/component/meter-gauge/meter-gauge.component.ts
@@ -1,47 +1,14 @@
-import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
-import { style, trigger, state, transition, animate } from '@angular/animations';
-
-export const scaleAnimation = (value) => {
-  return trigger(`scaleAnimation`, [
-    state('start', style({ transform: 'rotate(0)' })),
-    state('end', style({ transform: 'rotate(-180deg)' })),
-    transition('start => end', [style({ transform: 'rotate(-360deg)' }), animate('350ms ease-out')]),
-    transition('end => start', animate('350ms ease-in'))
-    // state('0', style({
-    //     overflow: 'hidden',
-    //     height: '*'
-    // })),
-    // state('1', style({
-    //     overflow: 'hidden',
-    //     height: `${height}px`
-    // })),
-    // transition('1 => 0', animate('400ms ease-in-out')),
-    // transition('0 => 1', animate('400ms ease-in-out'))
-  ]);
-};
+import { Component, Input, ViewChild, ElementRef, Renderer2, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-meter-gauge',
   templateUrl: './meter-gauge.component.html',
-  styleUrls: ['./meter-gauge.component.scss'],
-  animations: [trigger(`scaleAnimation`, [
-    state('start', style({ transform: 'rotate(-90deg)' })),
-    state('end', style({ transform: 'rotate(70deg)' })),
-    transition('start => end', [style({ transform: 'rotate(-360deg)' }), animate('350ms ease-out')]),
-    transition('end => start', animate('350ms ease-in'))
-    // state('0', style({
-    //     overflow: 'hidden',
-    //     height: '*'
-    // })),
-    // state('1', style({
-    //     overflow: 'hidden',
-    //     height: `${height}px`
-    // })),
-    // transition('1 => 0', animate('400ms ease-in-out')),
-    // transition('0 => 1', animate('400ms ease-in-out'))
-  ])]
+  styleUrls: ['./meter-gauge.component.scss']
 })
-export class MeterGaugeComponent implements OnInit, AfterViewInit {
+export class MeterGaugeComponent implements OnChanges {
+
+
+  @ViewChild('needle') needleRef: ElementRef;
 
   @Input()
   private maxValue: number;
@@ -50,61 +17,24 @@ export class MeterGaugeComponent implements OnInit, AfterViewInit {
   @Input()
   private value: number;
 
-  public calcValue = 0;
-  public rotateValueStr = '';
-  public translateValueStr = '';
-  public animationState = 'end';
+  public calcValue = -90;
 
-  constructor() { }
+  constructor(private renderer: Renderer2) {}
 
-  ngOnInit() {
-    const calcPercentage = (this.value * 100) / (this.maxValue - this.minValue);
-    this.calcValue = calcPercentage * 1.8 - 45;
-
-    // this.rotateValueStr = `rotate( ${this.value} deg)`;
-    // this.translateValueStr = `translate(-50%, -50%) rotate( ${this.value * 1.8} deg)`;
-    // $('.circle-inner, .gauge-copy').css({
-    //   'transform': 'rotate(' + newVal + 'deg)'
-    // });
-    // $('.gauge-copy').css({
-    //   'transform': 'translate(-50%, -50%) rotate(' + dVal * 1.8 + 'deg)'
-    // });
-
-    // $('.percentage').text(dVal + ' %');
-
-    // $(function(){
-    //   $('.text-box').keyup(function(){
-    //     if ($('.text-box').val() == '') {
-    //       $('.circle-inner, .gauge-copy').css({
-    //         'transform' : 'rotate(-45deg)'
-    //       });
-    //       $('.gauge-copy').css({
-    //         'transform' : 'translate(-50%, -50%) rotate(0deg)'
-    //       });
-    //       $('.percentage').text('0 %');
-    //     } else if($('.text-box').val() >= 0 && $('.text-box').val() <= 100) {
-    //       var dVal = $(this).val();
-    //       var newVal = dVal * 1.8 - 45;
-    //       $('.circle-inner, .gauge-copy').css({
-    //         'transform' : 'rotate(' + newVal + 'deg)'
-    //       });
-    //       $('.gauge-copy').css({
-    //         'transform' : 'translate(-50%, -50%) rotate(' + dVal * 1.8 + 'deg)'
-    //       });
-    //       $('.percentage').text(dVal + ' %');
-    //     } else {
-    //       $('.percentage').text('Invalid input value');
-    //     }
-    //   });
-    // });
+  ngOnChanges(changes: SimpleChanges): void {
+    this.reevaulateValues();
   }
 
-  ngAfterViewInit() {
+  private reevaulateValues() {
+    const percentage = (this.value * 100) / (this.maxValue - this.minValue);
+    this.calcValue = (1.8 * percentage) - 90;
+    this.updateGaugeAnimation(this.calcValue);
+  }
 
-    setTimeout(() => {
-      this.animationState = 'start';
-    }, 5000);
 
+
+  private updateGaugeAnimation(percentage: number) {
+    this.renderer.setStyle(this.needleRef.nativeElement, 'transform', `rotate(${percentage}deg)`);
   }
 
 }
