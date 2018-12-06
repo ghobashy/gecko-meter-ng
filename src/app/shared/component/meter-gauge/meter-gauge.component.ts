1f517641529207108337a460d8b6f2628d925b8d
Mohamed Ghobashy <mohamed.mostafa-ghobashy@vodafone.com>
2018-12-06 08:50:11 +0200

    [DEVELOP] Add counting up to meter values

--- src/app/shared/component/meter-gauge/meter-gauge.component.ts
+++ src/app/shared/component/meter-gauge/meter-gauge.component.ts
@@ -18,8 +18,28 @@ export class MeterGaugeComponent implements OnChanges {
   private value: number;
 
   public calcValue = -90;
+  public endValue;
+  public startValue = 0;
 
-  constructor(private renderer: Renderer2) {}
+  private options = {
+    useEasing: true,
+    useGrouping: true,
+    separator: ',',
+    decimal: '.',
+  };
+  private demo;
+
+  constructor(private renderer: Renderer2) {
+    // this.demo = new CountUp('myTargetElement', 0, 5036, 0, 2.5, options);
+    // if (!this.demo.error) {
+    //   this.demo.start();
+    // } else {
+    //   console.error(demo.error);
+    // }
+
+    // this.startValue = 0;
+    // this.endValue = 190;
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     this.reevaulateValues();
@@ -35,6 +55,12 @@ export class MeterGaugeComponent implements OnChanges {
 
   private updateGaugeAnimation(percentage: number) {
     this.renderer.setStyle(this.needleRef.nativeElement, 'transform', `rotate(${percentage}deg)`);
+    this.startValue = this.endValue;
+    this.endValue = this.value;
+    // setInterval(() => {
+    //   this.startValue = this.endValue;
+    //   this.endValue += this.value;
+    // }, 5000);
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MeterGaugeComponent } from './component/meter-gauge/meter-gauge.component';
 import { ButtonComponent } from './component/button/button.component';
+import { CountUpModule } from 'countup.js-angular2';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    CountUpModule
   ],
   exports: [
     MeterGaugeComponent,
-    ButtonComponent
+    ButtonComponent,
+    CountUpModule
   ],
   declarations: [MeterGaugeComponent, ButtonComponent]
 })
